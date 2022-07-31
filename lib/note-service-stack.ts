14664f46775c93413a493a3aae0bccab9c050163
Danielle Heberling <deeheber@users.noreply.github.com>
2022-07-31 13:47:41 -0700

    Minor refactor

--- lib/note-service-stack.ts
+++ lib/note-service-stack.ts
@@ -1,6 +1,6 @@
 import { Construct } from 'constructs';
 import { Stack, StackProps } from 'aws-cdk-lib';
-import { CfnOutput, RemovalPolicy } from 'aws-cdk-lib';
+import { CfnOutput, RemovalPolicy, aws_lambda as lambda } from 'aws-cdk-lib';
 import { AttributeType, BillingMode, Table } from 'aws-cdk-lib/aws-dynamodb';
 import { Architecture, Runtime } from 'aws-cdk-lib/aws-lambda';
 import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
@@ -11,127 +11,130 @@ export class NoteServiceStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
+    // Create GQL AppSync API
     const api = new GraphqlApi(this, 'NotesApi', {
       name: 'notes-api',
       logConfig: {
         fieldLogLevel: FieldLogLevel.ERROR,
       },
-      schema: Schema.fromAsset('src/graphql/schema.graphql')
+      schema: Schema.fromAsset('src/graphql/schema.graphql'),
     });
 
+    // Create DynamoDB table
     const notesTable = new Table(this, 'NotesTable', {
       billingMode: BillingMode.PAY_PER_REQUEST,
       partitionKey: { name: 'id', type: AttributeType.STRING },
       removalPolicy: RemovalPolicy.DESTROY,
-      tableName: 'notes-table'
+      tableName: 'notes-table',
     });
 
-    // create
-    const createLambda = new NodejsFunction(this, 'CreateLambda', {
+    // Create
+    const createLambda = this.createAppSyncLambda({
+      lambdaId: 'CreateLambda',
       functionName: 'create-lambda',
-      runtime: Runtime.NODEJS_16_X,
       entry: 'dist/src/functions/create.js',
-      logRetention: RetentionDays.ONE_WEEK,
-      architecture: Architecture.ARM_64,
-      memorySize: 3008,
+      environment: {
+        ['TABLE_NAME']: notesTable.tableName,
+      },
     });
 
-    const createDs = api.addLambdaDataSource('createDatasource', createLambda);
-
-    createDs.createResolver({
+    this.createResolverMappings({
+      api,
+      dataSourceName: 'createDatasource',
+      lambdaFunction: createLambda,
       typeName: 'Mutation',
-      fieldName: 'createNote'
+      fieldName: 'createNote',
     });
 
-    createLambda.addEnvironment('TABLE_NAME', notesTable.tableName);
     notesTable.grantWriteData(createLambda);
 
-    // list
-    const listLambda = new NodejsFunction(this, 'ListLambda', {
+    // List
+    const listLambda = this.createAppSyncLambda({
+      lambdaId: 'ListLambda',
       functionName: 'list-lambda',
-      runtime: Runtime.NODEJS_16_X,
       entry: 'dist/src/functions/list.js',
-      logRetention: RetentionDays.ONE_WEEK,
-      architecture: Architecture.ARM_64,
-      memorySize: 3008
+      environment: {
+        ['TABLE_NAME']: notesTable.tableName,
+      },
     });
 
-    const listDs = api.addLambdaDataSource('listDatasource', listLambda);
-
-    listDs.createResolver({
+    this.createResolverMappings({
+      api,
+      dataSourceName: 'listDatasource',
+      lambdaFunction: listLambda,
       typeName: 'Query',
-      fieldName: 'listNotes'
+      fieldName: 'listNotes',
     });
 
-    listLambda.addEnvironment('TABLE_NAME', notesTable.tableName);
     notesTable.grantReadData(listLambda);
 
-    // get
-    const getLambda = new NodejsFunction(this, 'GetLambda', {
+    // Get
+    const getLambda = this.createAppSyncLambda({
+      lambdaId: 'GetLambda',
       functionName: 'get-lambda',
-      runtime: Runtime.NODEJS_16_X,
       entry: 'dist/src/functions/get.js',
-      logRetention: RetentionDays.ONE_WEEK,
-      architecture: Architecture.ARM_64,
-      memorySize: 3008
+      environment: {
+        ['TABLE_NAME']: notesTable.tableName,
+      },
     });
 
-    const getDs = api.addLambdaDataSource('getDatasource', getLambda);
-
-    getDs.createResolver({
+    this.createResolverMappings({
+      api,
+      dataSourceName: 'getDatasource',
+      lambdaFunction: getLambda,
       typeName: 'Query',
-      fieldName: 'getNote'
+      fieldName: 'getNote',
     });
 
-    getLambda.addEnvironment('TABLE_NAME', notesTable.tableName);
     notesTable.grantReadData(getLambda);
 
-    // delete
-    const deleteLambda = new NodejsFunction(this, 'DeleteLambda', {
+    // Delete
+    const deleteLambda = this.createAppSyncLambda({
+      lambdaId: 'DeleteLambda',
       functionName: 'delete-lambda',
-      runtime: Runtime.NODEJS_16_X,
       entry: 'dist/src/functions/delete.js',
-      logRetention: RetentionDays.ONE_WEEK,
-      architecture: Architecture.ARM_64,
-      memorySize: 3008
+      environment: {
+        ['TABLE_NAME']: notesTable.tableName,
+      },
     });
 
-    const deleteDs = api.addLambdaDataSource('deleteDatasource', deleteLambda);
-
-    deleteDs.createResolver({
+    this.createResolverMappings({
+      api,
+      dataSourceName: 'deleteDatasource',
+      lambdaFunction: deleteLambda,
       typeName: 'Mutation',
-      fieldName: 'deleteNote'
+      fieldName: 'deleteNote',
     });
 
-    deleteLambda.addEnvironment('TABLE_NAME', notesTable.tableName);
     notesTable.grantWriteData(deleteLambda);
 
-    // update
-    const updateLambda = new NodejsFunction(this, 'UpdateLambda', {
+    // Update
+    const updateLambda = this.createAppSyncLambda({
+      lambdaId: 'UpdateLambda',
       functionName: 'update-lambda',
-      runtime: Runtime.NODEJS_16_X,
       entry: 'dist/src/functions/update.js',
-      logRetention: RetentionDays.ONE_WEEK,
-      architecture: Architecture.ARM_64,
-      memorySize: 3008
+      environment: {
+        ['TABLE_NAME']: notesTable.tableName,
+      },
     });
 
-    const updateDs = api.addLambdaDataSource('updateDatasource', updateLambda);
-
-    updateDs.createResolver({
+    this.createResolverMappings({
+      api,
+      dataSourceName: 'updateDatasource',
+      lambdaFunction: updateLambda,
       typeName: 'Mutation',
-      fieldName: 'updateNote'
+      fieldName: 'updateNote',
     });
 
-    updateLambda.addEnvironment('TABLE_NAME', notesTable.tableName);
     notesTable.grantWriteData(updateLambda);
 
+    // Output Stack Variables
     new CfnOutput(this, 'apiURL', {
-      value: api.graphqlUrl
+      value: api.graphqlUrl,
     });
 
     new CfnOutput(this, 'apiId', {
-      value: api.apiId
+      value: api.apiId,
     });
 
     /**
@@ -143,5 +146,46 @@ export class NoteServiceStack extends Stack {
     //     value: api.apiKey
     //   });
     // }
+  }
+
+  private createAppSyncLambda(params: {
+    lambdaId: string;
+    functionName: string;
+    entry: string;
+    environment: {
+      [key: string]: string;
+    };
+  }): lambda.Function {
+    const { lambdaId, functionName, entry, environment } = params;
+
+    return new NodejsFunction(this, `${lambdaId}`, {
+      functionName,
+      runtime: Runtime.NODEJS_16_X,
+      entry,
+      logRetention: RetentionDays.ONE_WEEK,
+      architecture: Architecture.ARM_64,
+      memorySize: 3008,
+      environment,
+    });
+  }
+
+  private createResolverMappings(params: {
+    api: GraphqlApi;
+    dataSourceName: string;
+    lambdaFunction: lambda.Function;
+    typeName: string;
+    fieldName: string;
+  }): void {
+    const { api, dataSourceName, lambdaFunction, typeName, fieldName } = params;
+
+    const dataSource = api.addLambdaDataSource(
+      `${dataSourceName}`,
+      lambdaFunction
+    );
+
+    dataSource.createResolver({
+      typeName,
+      fieldName,
+    });
   }
 }
